6d35129d018630317711ead18526fb4f905e3834
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-28 11:40:28 -0400

    Created remove method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -23,6 +23,7 @@ public class MyLinkedList<E> {
 				tail = next;
 			}
 		}
+		size++;
 	}
 	public void merge(MyLinkedList<E> l1, MyLinkedList<E> l2) {
 		l1.tail.next = l2.head;
@@ -36,10 +37,31 @@ public class MyLinkedList<E> {
 	}
 	public E pop() {
 		if (head != null) {
+			size--;
 			E data = head.data;
 			head = head.next;
 			return data;
 		}
 		return null;
+	}
+	public E remove(int index) {
+		if (index < size) {
+			if (index == 0) return pop();
+			size--;
+			Node<E> cur = head.next;
+			Node<E> prev = head;
+			Node<E> next = cur.next;
+			for (int i = 1; i < index; i++) {
+				cur = next;
+				prev = cur;
+				next = cur.next;
+			}
+			E data = cur.data;
+			prev.next = next;
+			if (index == size - 1) tail = prev;
+			return data;
+		}
+		else 
+			throw new IllegalArgumentException();
 	}
 }
