c8d1a0482bba6a4535cf2488fb0709ac48fc7dab
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-28 11:13:00 -0400

    Started both LinkedList and Node classes

--- LinkedList.java
+++ LinkedList.java
@@ -2,8 +2,11 @@
 public class LinkedList<E> {
 	private Node<E> head;
 	private Node<E> tail;
+	private int size;
 	public LinkedList() {
-		
+		head = null;
+		tail = null;
+		size = 0;
 	}
 	public void add(E element) {
 		
