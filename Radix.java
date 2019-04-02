08c2c7c2f7f172324bb80af364ddfc1a0310f98f
Kiran Duggirala <kduggirala10@stuy.edu>
2019-04-02 10:48:41 -0400

    Tested radixsort vs. builtin, my sort inefficient

--- Radix.java
+++ Radix.java
@@ -1,15 +1,40 @@
-
+import java.util.Arrays;
 public class Radix {
-	public static void main(String[] args) { 
-		int[] data = new int[20];
-		for (int i = 0; i < 20; i++) {
-			data[i] = (int) (Math.random() * 1000 - 500);
+	public static void main(String[]args){
+		  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+		  int[]MAX_LIST = {1000000000,500,10};
+		  for(int MAX : MAX_LIST){
+		    for(int size = 31250; size < 2000001; size*=2){
+		      long qtime=0;
+		      long btime=0;
+		      //average of 5 sorts.
+		      for(int trial = 0 ; trial <=5; trial++){
+		        int []data1 = new int[size];
+		        int []data2 = new int[size];
+		        for(int i = 0; i < data1.length; i++){
+		          data1[i] = (int)(Math.random()*MAX);
+		          data2[i] = data1[i];
+		        }
+		        long t1,t2;
+		        t1 = System.currentTimeMillis();
+		        radixsort(data2);
+		        t2 = System.currentTimeMillis();
+		        qtime += t2 - t1;
+		        t1 = System.currentTimeMillis();
+		        Arrays.sort(data1);
+		        t2 = System.currentTimeMillis();
+		        btime+= t2 - t1;
+		        if(!Arrays.equals(data1,data2)){
+		          System.out.println("FAIL TO SORT!");
+		          System.exit(0);
+		        }
+		      }
+		      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+		    }
+		    System.out.println();
+		  }
 		}
-		radixsort(data);
-		for (int i : data) {
-			System.out.print(i + " ");
-		}
-	}
+
 	@SuppressWarnings("unchecked")
 	public static void radixsort(int[] data) {
 		MyLinkedList<Integer>[] digits = new MyLinkedList[20];
@@ -34,7 +59,6 @@ public class Radix {
 				digits[1].extend(digits[j]);
 			}
 			list.extend(digits[1]);
-			String s = list.toString();
 			digits[1].clear();
 		}
 		for (int i = 0; i < data.length; i++) {
